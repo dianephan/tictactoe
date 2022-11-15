586dba18b11241d7012ebe930d95e6b22cc981e8
dIANE phan <dianephan12@gmail.com>
2022-11-15 10:48:14 -0800

    created function to move piece on the board

--- src/main/java/com/example/tictactoe/controller/GameRestController.java
+++ src/main/java/com/example/tictactoe/controller/GameRestController.java
@@ -2,7 +2,10 @@ package com.example.tictactoe.controller;
 
 import com.example.tictactoe.model.Game;
 import com.example.tictactoe.service.GameService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,7 @@ import java.util.Optional;
 
 @RestController
 public class GameRestController {
+    private static final Logger LOG = LoggerFactory.getLogger(GameRestController.class);
 
     private final GameService myGameService;
 
@@ -31,6 +35,27 @@ public class GameRestController {
     public ResponseEntity<String> getGame(@PathVariable("game-id") String gameId) {
         return ResponseEntity.of(myGameService
                 .getGame(gameId)
+                .map(Game::toString));
+    }
+
+    @PostMapping("/game/{game-id}/{piece}/{position}")
+    public ResponseEntity<String> movePiece(@PathVariable("game-id") String gameId,
+                                            @PathVariable("piece") String piece,
+                                            @PathVariable("position") Integer position) {
+        Optional<Game> currentGame = myGameService.getGame(gameId);
+//        if (currentGame.isPresent() && (!Objects.equals(piece, "X") || !piece.equals("O"))){
+        if ((!piece.equals("X")) && (!piece.equals("O")) && currentGame.isPresent()){
+            LOG.info("current game exists but input is not X or O");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("Must be X or O.");
+        }
+        if (currentGame.isPresent() && (position < 1 || position > 9)){
+            LOG.info("current game exists but position out of bound");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("Spot is out of bounds");
+        }
+        return ResponseEntity.of(myGameService
+                .movePiece(gameId, piece, position)
                 .map(Game::toString));
     }
 }
