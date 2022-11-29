312acde4a0d2b7a6ae94a0989484e6f8dd6a61f1
dIANE phan <dianephan12@gmail.com>
2022-11-29 15:55:44 -0800

    - added logic to handle quiz questions and answers - hardcoded the question and answers in new hashmap

--- src/main/java/com/example/tictactoe/model/Game.java
+++ src/main/java/com/example/tictactoe/model/Game.java
@@ -5,22 +5,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
     private static final Logger LOG = LoggerFactory.getLogger(Game.class);
 
     private final String id;
-
     private final Cell[] cells = new Cell[9];
-
     private GameState currentState;
-
     private int capacity = 0;
+    private final Map<Integer, Integer> questions = new HashMap<>();
 
     public Game(String id) {
         this.id = id;
         Arrays.fill(cells, Cell.EMPTY);
         currentState = GameState.X_TURN;
+        questions.put(1, 3);
+        questions.put(2, 1);
+        questions.put(3, 3);
+        questions.put(4, 2);
+        questions.put(5, 1);
+        questions.put(6, 3);
+        questions.put(7, 3);
+        questions.put(8, 4);
+        questions.put(9, 2);
     }
 
     public String getId() {
@@ -41,24 +50,31 @@ public class Game {
         return currentState;
     }
 
-    public boolean changeCell(Cell piece, Integer position) {
+    public boolean changeCell(Cell piece, Integer position, Integer answer) {
         if (!cells[position - 1].equals(Cell.EMPTY)) {
             // ideally should return this message in Controller
             LOG.info("Spot is taken.");
             return false;
         }
         if (currentState.equals(GameState.DRAW)
-            || currentState.equals(GameState.O_VICTORY)
-            || currentState.equals(GameState.X_VICTORY)) {
+                || currentState.equals(GameState.O_VICTORY)
+                || currentState.equals(GameState.X_VICTORY)) {
             return false;
         }
         if (pieceXturnO(piece) || pieceOturnX(piece)) {
             return false;
-        } else {
-            // you know that the piece is correct so time to
-            cells[position - 1] = piece;
-            capacity += 1;
-            currentState = currentState.switchTurns();
+        }
+        else {
+            // you know that the piece is correct and available so time to ask the question
+            if (answer.equals(questions.get(position))) {
+                LOG.info("Answer is correct!");
+                cells[position - 1] = piece;
+                capacity += 1;
+                currentState = currentState.switchTurns();
+            } else {
+                LOG.info("INCORRECT!");
+                return false;
+            }
             // if those surrounding the piece are same, then Victory
             if ((cells[0].equals(piece) && cells[1].equals(piece) && cells[2].equals(piece))
                     || (cells[3].equals(piece) && cells[4].equals(piece) && cells[5].equals(piece))
