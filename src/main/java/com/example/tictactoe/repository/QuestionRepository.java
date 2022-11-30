011c94822c5d8df57b12e2a0495b62643e0af4b3
dIANE phan <dianephan12@gmail.com>
2022-11-30 13:06:46 -0800

    - creating necessary files to handle the mysql db - adding new feature to the game

--- src/main/java/com/example/tictactoe/repository/QuestionRepository.java
+++ src/main/java/com/example/tictactoe/repository/QuestionRepository.java
@@ -1,2 +1,7 @@
-package com.example.tictactoe.repository;public class QuestionRepository {
+package com.example.tictactoe.repository;
+
+import com.example.tictactoe.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
